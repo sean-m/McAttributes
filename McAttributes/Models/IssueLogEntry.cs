02a1eda32df3d009f8faf8bec6058638cf5105f1
Sean McArdle <sean@mcardletech.com>
2022-11-08 15:39:13 -0800

    Update model.

--- McAttributes/Models/IssueLogEntry.cs
+++ McAttributes/Models/IssueLogEntry.cs
@@ -11,14 +11,16 @@ namespace McAttributes.Models
         public int Id { get; set; }
         [Column("attrname")]
         public string? AttrName { get; set; }
-        [Column("alerthash")]
-        public string? AlertHash { get; set; }
         [Column("created")]
         public DateTime? Created { get; set; }
+        [Column("alerthash")]
+        public string? AlertHash { get; set; }
         [Column("status")]
         public string? Status { get; set; }
         [Column("description")]
         public string? Description { get; set; }
+        [Column("notes")]
+        public string? Notes { get; set; }
     }
 
     public enum LogLevel
