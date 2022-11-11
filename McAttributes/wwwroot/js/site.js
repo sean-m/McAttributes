275a9e1cc7859c5f757ca4eaa2f0dc53e7286914
Sean McArdle <sean@mcardletech.com>
2022-11-10 17:21:41 -0800

    Working on Issue UI.

--- McAttributes/wwwroot/js/site.js
+++ McAttributes/wwwroot/js/site.js
@@ -2,3 +2,41 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            searchTerm: "",
+            includeResolved: false,
+            issues: {
+                count() { return this.issueList.count },
+                issueList: [],
+            },
+            currentIssue: {
+                edit: false,
+                model: {}
+            }
+        }
+    },
+    methods: {
+        searchForIssues() {
+
+        },
+        newIssueEntry() {
+            this.currentIssue = {
+                edit: false,
+                model: {
+                    attrName:'',
+                    created:'',
+                    alertHash:'',
+                    status:'',
+                    description:'',
+                    notes:''
+                }
+            }
+        }
+    }
+}).mount('#app')
