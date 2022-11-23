5c1ea86551d933acfad7e07f05f54315f28e048f
Sean McArdle <sean@mcardletech.com>
2022-11-23 08:43:02 -0800

    This controller doesn't do anything without authorization.

--- McAttributes/Controllers/MeController.cs
+++ McAttributes/Controllers/MeController.cs
@@ -6,6 +6,7 @@ using System.Security.Claims;
 namespace McAttributes.Controllers {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class MeController : ControllerBase {
         ILogger _logger;
         MeController(ILogger<MeController> logger) {
