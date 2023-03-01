c8fc2133f4abd29081ddc5c56633fc03d8d00d30
Sean McArdle <sean@mcardletech.com>
2023-02-28 17:27:37 -0800

    Refactored data helpers into single file.

--- McAttributes/DebugInit.cs
+++ McAttributes/DebugInit.cs
@@ -128,86 +128,14 @@ create table if not exists IssueLogEntry (
         public static void DbInit(IdDbContext context) {
             if (context == null) throw new ArgumentNullException("Gonna hand me a viable databse connection.");
 
-            DbInit<Models.User>(context, @"./test_values.csv");
+            UglyDbInitHelper.DbInit<Models.User>(context, @"./test_values.csv");
         }
 
         public static void DbInit<T>(IdDbContext context) {
             if (context == null) throw new ArgumentNullException("Gonna hand me a viable databse connection.");
 
-            DbInit<T>(context, @"./test_values.csv");
+            UglyDbInitHelper.DbInit<T>(context, @"./test_values.csv");
         }
 
-        public static void DbInit<T>(IdDbContext context, string CsvPath) {
-            if (context == null) throw new ArgumentNullException("Gonna hand me a viable databse connection.");
-            if (!File.Exists(CsvPath)) throw new ArgumentNullException($"CsvFile path not exists {CsvPath}, do better.");
-
-
-            var csvReader = new CsvFileReader(CsvPath, true);
-            var _ = csvReader.ReadFileValues().FirstOrDefault();
-
-            var types = new Dictionary<string, PropertyInfo>();
-            var modelType = typeof(T);
-            if (modelType == null) { throw new Exception($"Can't get type info for {modelType.Name}, you got problems."); }
-
-            Type GetPropType(string name) {
-                if (types.ContainsKey(name)) return types[name].PropertyType;
-
-                var prop = modelType.GetProperties().FirstOrDefault(x => x.Name.ToString().Equals(name, StringComparison.CurrentCultureIgnoreCase));
-                if (prop == null) {
-                    throw new Exception($"Cannot resolve property with name: {name} on class '{modelType.Name}'");
-                }
-
-                types.Add(name, prop);
-                return prop.PropertyType;
-            }
-
-            PropertyInfo GetPropInfo(string name) {
-                if (types.ContainsKey(name)) return types[name];
-
-                var prop = modelType.GetProperties().FirstOrDefault(x => x.Name.ToString().Equals(name, StringComparison.CurrentCultureIgnoreCase));
-                if (prop == null) {
-                    throw new Exception($"Cannot resolve property with name: {name} on class '{modelType.Name}'");
-                }
-
-                types.Add(name, prop);
-                return prop;
-            }
-
-            csvReader = new CsvFileReader(CsvPath;
-            var userType = typeof(Models.User);
-
-            foreach (var row in csvReader.ReadFileValues()) {
-                var columns = row.Keys.Where(k => !String.IsNullOrEmpty(row.GetValueOrDefault(k)));
-
-                var record = Activator.CreateInstance(modelType);
-                foreach (var col in columns) {
-                    var prop = GetPropInfo(col);
-                    var type = GetPropType(col);
-                    object value = GetAsType(row[col], type);
-                    prop.SetValue(record, value);
-                }
-                if (record != null) context.Add(Convert.ChangeType(record, modelType));
-            }
-
-            context.SaveChanges();
-        }
-
-
-        static object? GetAsType(object source, Type desiredType) {
-            if (source == null) return source;
-
-            string strSrc = source.ToString();
-            if (string.IsNullOrEmpty(strSrc)) {
-                  return null;
-            }
-
-            if (desiredType == typeof(Guid)) {
-                return Guid.Parse(source.ToString());
-            } else if (desiredType == typeof(DateTime?)) {
-                return DateTime.Parse(source.ToString());
-            } 
-
-            return Convert.ChangeType(source, desiredType);  
-        }
     }
 }
