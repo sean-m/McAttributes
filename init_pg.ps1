1f7e564c868aadc3b4d32de3b5e8f2acdc6eaca8
Sean McArdle <sean@mcardletech.com>
2023-08-25 16:53:17 -0700

    Synced with current prod.

--- init_pg.ps1
+++ init_pg.ps1
@@ -101,11 +101,10 @@ CREATE DATABASE identity
 "@ 
 
     $sqlQuery = @"
-
 create table if not exists graphraw (
-	id serial primary key,
+	id bigserial primary key,
 	created timestamp,
-	dataSourceId TEXT,
+	dataSourceId varchar(128),
 	tenant text,
 	graphRecord jsonb
 );
@@ -121,10 +120,10 @@ create table if not exists hr (
 create table if not exists dataLog (
 	id serial primary key,
 	created timestamp DEFAULT NOW(),
-	logType TEXT,
-	message TEXT,
-	file TEXT,
-	jobId TEXT
+	logType varchar(64),
+	message varchar(1024),
+	file varchar(1024),
+	jobId varchar(64)
 );
 
 create table if not exists wdusers (
@@ -138,14 +137,16 @@ create table if not exists wdusers (
 );
 
 CREATE TABLE if NOT EXISTS azusers (
- id SERIAL PRIMARY KEY,
+ id BIGSERIAL PRIMARY KEY,
  merged TIMESTAMP,
  modified TIMESTAMP,
  lastFetched TIMESTAMP,
  created TIMESTAMP,
  enabled BOOLEAN,
+ onPremisesSyncEnabled BOOLEAN,
  tenant TEXT,
  aadId UUID UNIQUE,
+ refersTo UUID,
  upn TEXT,
  mail TEXT,
  employeeId TEXT,
@@ -158,7 +159,8 @@ CREATE TABLE if NOT EXISTS azusers (
  preferredGivenName TEXT,
  preferredSurname TEXT,
  articles TEXT,
- pronouns TEXT
+ pronouns TEXT,
+ otherMails varchar(256)[]
 );
 CREATE INDEX IF NOT EXISTS idx_azusers_aadid ON azusers(aadId);
 CREATE INDEX IF NOT EXISTS idx_azusers_mail ON azusers(mail);
@@ -179,6 +181,7 @@ CREATE INDEX IF NOT EXISTS idx_empid_upn ON employeeIdJoin(upn);
 
 CREATE TABLE IF NOT EXISTS idAlerts (
 	id SERIAL PRIMARY KEY,
+	created TIMESTAMP DEFAULT NOW(),
 	attrName VARCHAR(64),
 	alertHash VARCHAR(64) UNIQUE,
 	created TIMESTAMP,
@@ -204,18 +207,35 @@ CREATE TABLE IF NOT EXISTS azgroups (
 );
 
 CREATE TABLE IF NOT EXISTS azgroupmembers (
-	id SERIAL PRIMARY KEY,
-	partition VARCHAR(64) NOT NULL,
-	azgroupId VARCHAR(40) NOT NULL,
-	azgroupName VARCHAR(256) NOT NULL,
-	memberId INTEGER NOT NULL,
-	memberType VARCHAR(12),
+	id 				SERIAL PRIMARY KEY,
+	partition 		VARCHAR(64) NOT NULL,
+	azgroupId 		VARCHAR(40) NOT NULL,
+	azgroupName 	VARCHAR(256) NOT NULL,
+	memberId 		VARCHAR(256) NOT NULL,
+	memberGlobalId INTEGER,
+	memberType 		VARCHAR(12),
 	UNIQUE (azgroupId, memberId)
 );
 
 CREATE INDEX IF NOT EXISTS idx_azgrpmember_groupId ON azgroupmembers (azgroupid);
 CREATE INDEX IF NOT EXISTS idx_azgrpmember_groupName ON azgroupmembers (azgroupName);
 CREATE INDEX IF NOT EXISTS idx_azgrpmember_groupmember ON azgroupmembers (memberId);
+
+
+CREATE TABLE IF NOT EXISTS azgroupmembersimport (
+	id 			SERIAL PRIMARY KEY,
+	partition 	VARCHAR(64) NOT NULL,
+	azgroupId 	VARCHAR(40) NOT NULL,
+	azgroupName VARCHAR(256) NOT NULL,
+	memberId 	UUID NOT NULL,
+	memberType 	VARCHAR(12),
+	UNIQUE (azgroupId, memberId)
+);
+
+CREATE INDEX IF NOT EXISTS idx_azgroupmembersimport_partition ON azgroupmembersimport (partition);
+CREATE INDEX IF NOT EXISTS idx_azgroupmembersimport_groupId ON azgroupmembersimport (azgroupid);
+CREATE INDEX IF NOT EXISTS idx_azgroupmembersimport_groupName ON azgroupmembersimport (azgroupName);
+CREATE INDEX IF NOT EXISTS idx_azgroupmembersimport_groupmember ON azgroupmembersimport (memberId);
 
 
 /*
@@ -230,13 +250,13 @@ enum recordType {
 }
 */
 CREATE TABLE IF NOT EXISTS stargate (
-	id SERIAL PRIMARY KEY,
+	id BIGSERIAL PRIMARY KEY,
 	recordType INTEGER,
-	globalId INTEGER,
+	globalId BIGINT,
 	localId VARCHAR(256) NOT NULL UNIQUE,
 	upn VARCHAR(256),
 	partition VARCHAR(256),
-	joinSeed VARCHAR(256),
+	joinSeed VARCHAR(256) UNIQUE,
 	
 	CONSTRAINT globalId_fk 
 		FOREIGN KEY (globalId)
@@ -244,8 +264,7 @@ CREATE TABLE IF NOT EXISTS stargate (
 );
 CREATE INDEX IF NOT EXISTS idx_sg_joinSeed ON stargate(joinSeed);
 CREATE INDEX IF NOT EXISTS idx_sg_localId ON stargate(localId);
-CREATE INDEX IF NOT EXISTS idx_sg_globalId ON stargate(globalId);
-CREATE INDEX IF NOT EXISTS idx_sg_partitionId ON stargate(partition);
+CREATE INDEX IF NOT EXISTS idx_sg_globalId ON stargate(localId);
 CREATE INDEX IF NOT EXISTS idx_sg_upn ON stargate(upn);
 
 
@@ -293,59 +312,158 @@ AS `$$
 LANGUAGE 'sql'
 ;
 
-DROP FUNCTION IF EXISTS getAzureGroupMembersPartitionCrosswalk(GroupName VARCHAR(256), Partition VARCHAR(256));
-CREATE OR REPLACE FUNCTION getAzureGroupMembersPartitionCrosswalk(GroupName VARCHAR(256), Partition VARCHAR(256)) RETURNS TABLE (azGroupId VARCHAR(40), mappedId varchar(40))
-AS `$$
-	SELECT azGroupId, partitionCrosswalk(memberId, `$2::TEXT)::varchar FROM azgroupmembers
-`$$
-LANGUAGE 'sql'
-;
-
-
-DROP FUNCTION IF EXISTS getTotalNativeGroupMemberGlobalIds(GroupName VARCHAR(256));
-CREATE OR REPLACE FUNCTION getTotalNativeGroupMemberGlobalIds(GroupName VARCHAR(256)) RETURNS TABLE (globalId INTEGER)
-AS `$$
-	SELECT azGroup.memberId FROM azgroupmembers as azGroup
-	JOIN stargate AS sg1
-	ON azGroup.memberId = sg1.globalId
-	AND azGroup."partition" = sg1."partition"
-	AND azGroup.azgroupname = `$1
-	AND sg1.recordtype = 1 -- Get the group members that are native to the partition they are a member in. These are the users we actually want to replicate.
-`$$
-LANGUAGE 'sql'
-;
-
-/*
-Given a group name and partition that you care about, get the list of actions to perform on the group members.
-Results are returned as globalIds so they must still be crosswalked. 
-*/
-DROP FUNCTION IF EXISTS getNeededGroupMemberChanges(GroupName VARCHAR(256), Parititon VARCHAR(256));
-CREATE OR REPLACE FUNCTION getNeededGroupMemberChanges(GroupName VARCHAR(256), Partition VARCHAR(256)) RETURNS TABLE (groupId TEXT, memberId TEXT, partition VARCHAR(256), change VARCHAR(256))
-AS `$$
-
-WITH changes AS (
-	-- Get the list of users to remove from a given group in a given partition
-	SELECT memberId, `$2 AS partition, 'remove' AS change FROM azgroupmembers WHERE  azgroupname = `$1 AND partition = `$2
-	EXCEPT
-	SELECT globalId AS memberId, `$2 AS partition, 'remove' AS change FROM gettotalnativegroupmemberglobalids(`$1)
-	UNION
-	-- Get the list of users to add to a given group in a given partition
-	SELECT globalId AS memberId, `$2 AS partition, 'add' AS change FROM gettotalnativegroupmemberglobalids(`$1)
-	EXCEPT 
-	SELECT memberId, `$2 AS partition, 'add' AS change FROM azgroupmembers WHERE  azgroupname = `$1 AND partition = `$2
-)
-
-SELECT (SELECT azgroupid FROM azgroupmembers WHERE azgroupname = `$1 AND partition = `$2 LIMIT 1) AS groupId, m.partitioncrosswalk AS memberId, m.partition, m.change  FROM (SELECT partitioncrosswalk(memberId, `$2), partition, change FROM changes) m
-
-`$$
-LANGUAGE 'sql'
-;
 
 
 DROP FUNCTION IF EXISTS objIdToGlobalId (VARCHAR(256));
 CREATE OR REPLACE FUNCTION objIdToGlobalId (VARCHAR(256)) RETURNS INTEGER
 AS `$$
 	SELECT globalId FROM stargate WHERE localId = `$1
+`$$
+LANGUAGE 'sql'
+;
+
+DROP FUNCTION IF EXISTS objIdToGlobalIdAndType (VARCHAR(256));
+CREATE OR REPLACE FUNCTION objIdToGlobalIdAndType (VARCHAR(256)) RETURNS TABLE (globalId INTEGER, recordType INTEGER)
+AS `$$
+	SELECT globalId, recordType FROM stargate WHERE localId = `$1
+`$$
+LANGUAGE 'sql'
+;
+
+
+
+/*
+ *   Group Tables and Functions
+ */
+
+--drop table if exists FederatedGroup cascade;
+create table if not exists FederatedGroup (
+	id BIGSERIAL primary key
+	,displayName varchar(256) unique
+);
+
+--drop table if exists FederatedGroupMembership cascade;
+create table if not exists FederatedGroupMembership (
+	id BIGSERIAL primary key
+	,groupId BIGSERIAL
+	,globalId BIGSERIAL
+	
+	,CONSTRAINT globalId_fk 
+		FOREIGN KEY (globalId)
+		REFERENCES stargate(id)
+		
+	,constraint groupId_fk
+		foreign key (groupId)
+		references FederatedGroup(id)
+		on delete cascade
+);
+create unique index if not exists groupId_globalId_uniq on FederatedGroupMembership(groupId, globalId);
+
+--drop table if exists LocalGroup cascade;
+create table if not exists LocalGroup (
+	id BIGSERIAL primary key
+	,groupId BIGSERIAL
+	,localId varchar(256) unique
+	,partition varchar(64)
+	
+	,constraint groupId_fk
+		foreign key (groupId)
+		references FederatedGroup(id)
+		on delete cascade
+);
+
+--drop table if exists LocalGroupMembership cascade;
+create table if not exists LocalGroupMembership (
+	id BIGSERIAL primary key
+	,localGroupId BIGSERIAL
+	,globalId BIGSERIAL
+	,globalMemberType integer
+	,localId varchar(256)
+	,partition varchar(64)
+	,localOnly boolean default false
+	
+	,CONSTRAINT globalId_fk 
+		FOREIGN KEY (globalId)
+		REFERENCES stargate(id)
+		
+	,constraint localGroup_fk
+		foreign key (localGroupId)
+		references LocalGroup(id)
+		on delete cascade
+);
+create unique index if not exists localGroup_localId_uniq on LocalGroupMembership(localGroupId, localId);
+ 
+DROP FUNCTION IF EXISTS getNeededStandardGroupMemberChanges(GroupName VARCHAR(256), Parititon VARCHAR(256));
+CREATE OR REPLACE FUNCTION getNeededStandardGroupMemberChanges(GroupName VARCHAR(256), Partition VARCHAR(256)) RETURNS TABLE (groupId TEXT, memberId TEXT, upn VARCHAR(256), partition VARCHAR(256), change VARCHAR(256)) --, globalMemberType integer)
+AS `$$
+	-- Get list of federated group memberships for the specified group name.
+	-- Federated member records can only be contributed by a native account
+	-- in one of the contributing tenants, B2B accounts or 'guests' do not
+	-- contribute to the federated member set by convention. 
+	with recursive FederatedMembers as (
+		select fgm.* from FederatedGroupMembership as fgm
+		join FederatedGroup as fg
+		on fgm.groupId = fg.id 
+		where fg.displayname = `$1
+	),
+	SelectedLocalgroup as (
+		select id, groupid, localid, partition from LocalGroup 
+	   where groupId = (select groupid from FederatedMembers limit 1) 
+	   and partition = `$2
+	   limit 1
+	),
+	NativeLocalMembers as (
+		-- Get native and guest member records and join that with the federated members.
+		select lgm.* from LocalGroupMembership as lgm
+		join SelectedLocalGroup as lg
+		on lgm.localGroupId = lg.id
+			and lgm.partition = `$2
+		where lgm.globalmembertype < 30	
+	),
+	MembershipChanges as (
+		-- Get list of federated group memberships for the specified group name.
+		-- Federated member records can only be contributed by a native account
+		-- in one of the contributing tenants, B2B accounts or 'guests' do not
+		-- contribute to the federated member set by convention.
+		select case when fm.globalId is null then lgm.globalid else fm.globalId end as globalId -- Capture the global id of the member in question.
+			,case when fm.globalId is null then 'remove' else 'add' end as "action"           -- Use null globalId values in the federated or local member set to infer an add or remove action.
+			                                                                                  -- Membership records that exist in both sets are not considered. No changes needed.
+			,lgm.globalmembertype
+		from FederatedMembers as fm
+		full join NativeLocalMembers as lgm
+		on fm.globalid = lgm.globalid
+		where (fm.globalId is null
+			or lgm.globalid is null)
+	),
+	LocalMappedMembers as (
+		/*
+		Join local group member records with stargate as the local member
+		records are the stargate record local to the partition, they don't
+		carry the reference to an account's prime record which is needed
+		when comparing with the federated member record.
+		
+		Foo user account with guests in Bar and Baz mapped through
+		a prime record. The prime record's id is the globalid value for the
+		correlated records regardless of record type.
+		
+		           prime, 
+		             0
+		     ________|_________
+		    /        |         \
+		  10         20         20
+		 /           |           \
+		Foo         Bar          Baz
+		*/		
+		select * from MembershipChanges as mc
+		join stargate as sg
+		on sg.globalid = mc.globalid
+		and sg."partition" = `$2
+	)
+
+
+	select lg.localId as groupid, lmm.localid, lmm.upn, lmm."partition", lmm."action" from LocalMappedMembers as lmm
+	join SelectedLocalGroup as lg
+	on lg.partition = lmm.partition
 `$$
 LANGUAGE 'sql'
 ;
