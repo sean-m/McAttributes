52d84ffc96632ea5b56fe0031b6e97046dd3d462
Sean McArdle <sean@mcardletech.com>
2023-09-01 12:22:05 -0700

    Using McRule expression filter for typesafe search.

--- McAttributes/Pages/Users/Index.cshtml.cs
+++ McAttributes/Pages/Users/Index.cshtml.cs
@@ -7,26 +7,50 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using McAttributes.Data;
 using McAttributes.Models;
+using System.Linq.Expressions;
+using McRule;
 
 namespace McAttributes.Pages.Users
 {
     public class IndexModel : PageModel
     {
-        private readonly McAttributes.Data.IdDbContext _context;
+        private readonly IdDbContext _context;
 
-        public IndexModel(McAttributes.Data.IdDbContext context)
+        public IndexModel(IdDbContext context)
         {
             _context = context;
         }
 
-        public IList<User> User { get;set; } = default!;
+        public string SearchCriteria => (string)TempData[nameof(SearchCriteria)] ?? String.Empty;
+
+        public IList<User> UserSet { get;set; } = default!;
 
         public async Task OnGetAsync()
         {
             if (_context.Users != null)
             {
-                User = await _context.Users.ToListAsync();
+                UserSet = await _context.Users.Where(GetUserFilter()).ToListAsync();
             }
+        }
+
+        public IActionResult OnPost([FromForm] string SearchCriteria) {
+            TempData[nameof(SearchCriteria)] = SearchCriteria;
+            return RedirectToPage("Index");
+        }
+
+        private Expression<Func<User,bool>> GetUserFilter () {
+            if (String.IsNullOrEmpty(SearchCriteria)) return McRule.PredicateBuilder.False<User>();
+
+            var filter = new McRule.ExpressionRuleCollection();
+            filter.RuleOperator = McRule.PredicateExpressionPolicyExtensions.RuleOperator.Or;
+            filter.Rules = new List<McRule.IExpressionRule>() {
+                new McRule.ExpressionRule((nameof(Models.User), nameof(Models.User.Mail), SearchCriteria)),
+                new McRule.ExpressionRule((nameof(Models.User), nameof(Models.User.EmployeeId), SearchCriteria)),
+                new McRule.ExpressionRule((nameof(Models.User), nameof(Models.User.PreferredGivenName), SearchCriteria)),
+                new McRule.ExpressionRule((nameof(Models.User), nameof(Models.User.PreferredSurname), SearchCriteria)),
+            };
+            
+            return PredicateExpressionPolicyExtensions.GetEFPredicateExpression<User>(filter) ?? McRule.PredicateBuilder.False<User>();
         }
     }
 }
