94f2bcd69eccb7f5c5731404b94783c6c4e4d0b7
Sean McArdle <sean@mcardletech.com>
2024-08-01 17:40:59 -0700

    Added column for signinactivity.

--- McAttributes/Models/User.cs
+++ McAttributes/Models/User.cs
@@ -7,19 +7,19 @@ namespace McAttributes.Models {
     [Table("azusers")]
     [Index(nameof(AadId), nameof(Mail), nameof(EmployeeId))]
     public class User : RowVersionedModel {
-        
+
         [Column("lastfetched")]
         public DateTime? LastFetched { get; set; } = null;
-        
+
         [Column("merged")]
         public DateTime? Merged { set; get; } = null;
-        
+
         [Column("modified")]
         public DateTime? Modified { get; set; } = null;
-        
+
         [Column("created")]
         public DateTime? Created { get; set; } = null;
-        
+
         [Column("enabled")]
         public bool Enabled { get; set; }
 
@@ -28,34 +28,34 @@ namespace McAttributes.Models {
 
         [Column("tenant")]
         public string? Tenant { get; set; }
-        
+
         [Column("aadid")]
         public Guid AadId { get; set; }
-        
+
         [Column("upn")]
         public string? Upn { get; set; }
-        
+
         [Column("mail")]
         public string? Mail { get; set; }
-        
+
         [Column("displayname")]
         public string? DisplayName { get; set; }
-        
+
         [Column("employeeid")]
         public string? EmployeeId { get; set; }
-        
+
         [Column("ademployeeid")]
         public string? AdEmployeeId { get; set; }
-        
+
         [Column("hremployeeid")]
         public string? HrEmployeeId { get; set; }
-        
+
         [Column("wid")]
         public string? Wid { get; set; }
-        
+
         [Column("creationtype")]
         public string? CreationType { get; set; }
-        
+
         [Column("company")]
         public string? Company { get; set; }
 
@@ -64,19 +64,22 @@ namespace McAttributes.Models {
 
         [Column("preferredgivenname")]
         public string? PreferredGivenName { get; set; }
-        
+
         [Column("preferredsurname")]
         public string? PreferredSurname { get; set; }
-        
+
         [Column("articles")]
         [StringLength(64)]
         public string? Articles { get; set; }
-        
+
         [Column("pronouns")]
         [StringLength(24)]
         public string? Pronouns { get; set; }
 
         [Column("onpremisedn")]
         public string? OnPremiseDn { get; set; }
+
+        [Column("signinactivity", TypeName = "jsonb")]
+        public Dictionary<string,string>? SigninActivity { get; set; }
     }
 }
