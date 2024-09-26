6141ea2bbc9fbc28738d31719a277b9a4ff5cd20
Sean McArdle <sean@mcardletech.com>
2024-09-25 17:01:18 -0700

    Added an index to identify where it blows up while ingesting malformed data.

--- McAttributes/SMM/CsvFileReader.cs
+++ McAttributes/SMM/CsvFileReader.cs
@@ -2,12 +2,9 @@
 using System.Text.RegularExpressions;
 using Microsoft.VisualBasic.FileIO;
 
-namespace SMM
-{
-    public class CsvFileReader {
-        static string csv_delimiter_pattern = @"(?:^|,)(?=[^""]|("")?)""?((?(1)(?:[^""]|"""")*|[^,""]*))""?(?=,|$)";
-        static Regex csv_delimiter = new Regex(csv_delimiter_pattern, RegexOptions.Compiled);
 
+namespace SMM {
+    public class CsvFileReader {
         string file_path;
 
         private CsvFileReader(string FilePath) {
@@ -78,16 +75,22 @@ namespace SMM
         }
 
         public static IEnumerable<dynamic> GetRecordsDynamic(string CsvPath) {
+            int index = 0;
             foreach (var r in GetRecords(CsvPath)) {
+                index++;
                 var record = new System.Dynamic.ExpandoObject();
-                foreach (var kv in r) {
-                    ((ICollection<KeyValuePair<String, Object>>)record).Add(new KeyValuePair<string, object>(kv.Key, kv.Value));
+                try {
+                    foreach (var kv in r) {
+                        ((ICollection<KeyValuePair<String, Object>>)record).Add(new KeyValuePair<string, object>(kv.Key, kv.Value));
+                    }
+                } catch (Exception e) {
+                    throw new Exception($"Blew up on record {index}", e);
                 }
                 yield return record;
             }
         }
 
-        public static IEnumerable<T> GetRecords<T>(string CsvPath) {
+        public static IEnumerable<T> GetRecords<T>(string CsvPath, bool JustDoWhatYouCan = false) {
             if (!File.Exists(CsvPath)) throw new ArgumentNullException($"CsvFile path not exists {CsvPath}, do better. CD: {System.Environment.CurrentDirectory}");
 
             var csvReader = new CsvFileReader(CsvPath, true);
@@ -97,24 +100,24 @@ namespace SMM
             var modelType = typeof(T);
             if (modelType == null) { throw new Exception($"Can't get type info for {modelType.Name}, you got problems."); }
 
-            Type GetPropType(string name) {
+            Type? GetPropType(string name) {
                 if (types.ContainsKey(name)) return types[name].PropertyType;
 
                 var prop = modelType.GetProperties().FirstOrDefault(x => x.Name.ToString().Equals(name, StringComparison.CurrentCultureIgnoreCase));
                 if (prop == null) {
-                    throw new Exception($"Cannot resolve property with name: {name} on class '{modelType.Name}'");
+                    return null;
                 }
 
                 types.Add(name, prop);
                 return prop.PropertyType;
             }
 
-            PropertyInfo GetPropInfo(string name) {
+            PropertyInfo? GetPropInfo(string name) {
                 if (types.ContainsKey(name)) return types[name];
 
                 var prop = modelType.GetProperties().FirstOrDefault(x => x.Name.ToString().Equals(name, StringComparison.CurrentCultureIgnoreCase));
                 if (prop == null) {
-                    throw new Exception($"Cannot resolve property with name: {name} on class '{modelType.Name}'");
+                    return null;
                 }
 
                 types.Add(name, prop);
@@ -124,15 +127,30 @@ namespace SMM
             csvReader = new CsvFileReader(CsvPath);
             var userType = typeof(T);
 
+            int rindex = 0;
             foreach (var row in csvReader.ReadFileValues()) {
+                rindex++;
                 var columns = row.Keys.Where(k => !String.IsNullOrEmpty(row.GetValueOrDefault(k)));
 
+                int cindex = 1;
                 var record = Activator.CreateInstance(modelType);
-                foreach (var col in columns) {
-                    var prop = GetPropInfo(col);
-                    var type = GetPropType(col);
-                    object value = GetValueAsType(row[col], type);
-                    prop.SetValue(record, value);
+                try {
+                    foreach (var col in columns) {
+                        try {
+                            var prop = GetPropInfo(col);
+                            var type = GetPropType(col);
+                            if (prop == null || type == null) { continue; }
+                            object value = GetValueAsType(row[col], type);
+                            prop.SetValue(record, value);
+                        } catch (Exception e) {
+                            if (!JustDoWhatYouCan) {
+                                throw new Exception($"Encountered error handling property {col}", e);
+                            }
+                        }
+                    }
+                    cindex++;
+                } catch (Exception e) {
+                    throw new Exception($"Blew up on record {rindex} column {cindex}", e);
                 }
                 yield return (T)record;
             }
@@ -158,5 +176,7 @@ namespace SMM
 
             return Convert.ChangeType(source, desiredType);
         }
+
     }
+
 }
