0335e1d55d979d18d9c7956695cef270c520ae2f
Sean McArdle <sean@mcardletech.com>
2026-05-18 19:47:51 -0700

    These values don't really change anyway

--- McAttributes/Models/whoami.cs
+++ McAttributes/Models/whoami.cs
@@ -1,9 +1,14 @@
-﻿namespace McAttributes.Models {
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Identity.Client;
+
+namespace McAttributes.Models {
     public class whoami {
-        public string Region { get; } = Environment.GetEnvironmentVariable("REGION") ?? "unknown";
-        public string Replica { get; } = Environment.GetEnvironmentVariable("CONTAINER_APP_REPLICA_NAME")
+        public static string Region { get; } = Environment.GetEnvironmentVariable("REGION") ?? "unknown";
+        public static string Replica { get; } = Environment.GetEnvironmentVariable("CONTAINER_APP_REPLICA_NAME")
                     ?? Environment.MachineName
                     ?? "unknown";
-        public string Revision { get; } = Environment.GetEnvironmentVariable("CONTAINER_APP_REVISION") ?? "unknown";
+        public static string Revision { get; } = Environment.GetEnvironmentVariable("CONTAINER_APP_REVISION") ?? "unknown";
+
+        public static whoami Instance { get; } = new whoami(); // These values don't change anyhow
     }
 }
